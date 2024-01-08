3d50a45d249835847ddffb7b2b3e4c9e7551a1ca
Antoni Nowinowski <antoninowinowski@hotmail.com>
2024-01-08 12:25:21 +0100

    Add training code

--- src/lcmr_ext/utils/collate_fn.py
+++ src/lcmr_ext/utils/collate_fn.py
@@ -4,4 +4,6 @@ import torch
 def collate_fn(batch):
     if type(batch[0]) is tuple:
         batch = [list(x) for x in zip(*batch)]
-    return [torch.cat(x, dim=0).pin_memory() for x in batch]
+        return [torch.cat(x, dim=0).pin_memory() for x in batch]
+    else:
+        return torch.cat(batch, dim=0).pin_memory()
